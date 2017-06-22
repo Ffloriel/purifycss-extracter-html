9601f1dcaf5d8b041f0080583035476a39114c34
Ffloriel <florielfedry@gmail.com>
2017-06-22 22:40:12 +0100

    Add tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,10 +4,30 @@ import { TEST_1_CONTENT, TEST_1_TAG, TEST_1_CLASS, TEST_1_ID } from "./data"
 describe("purifyhtml", () => {
     it("finds tag selectors", () => {
         const received = purifyhtml.extract(TEST_1_CONTENT)
-        console.log(received)
+        for (let item of TEST_1_TAG) {
+            expect(received.includes(item)).toBe(true)
+        }
     })
 
-    it("finds classes selectors", () => {})
+    it("finds classes selectors", () => {
+        const received = purifyhtml.extract(TEST_1_CONTENT)
+        for (let item of TEST_1_CLASS) {
+            expect(received.includes(item)).toBe(true)
+        }
+    })
 
-    it("finds id selectors", () => {})
+    it("finds id selectors", () => {
+        const received = purifyhtml.extract(TEST_1_CONTENT)
+        for (let item of TEST_1_ID) {
+            expect(received.includes(item)).toBe(true)
+        }
+    })
+
+    it("finds all selectors", () => {
+        const received = purifyhtml.extract(TEST_1_CONTENT)
+        const selectors = [...TEST_1_TAG, ...TEST_1_CLASS, ...TEST_1_ID]
+        for (let item of TEST_1_TAG) {
+            expect(received.includes(item)).toBe(true)
+        }
+    })
 })
